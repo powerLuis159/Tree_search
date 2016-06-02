465f72460574156c6535ab901bfc14cdc7c4eaa7
Luis Ronald <L.Ronald.ic@outlook.com>
2016-06-02 05:17:37 -0500

    Depth search implementado!

---
+++ Tree search/Tree search/Ciudad.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "Ciudad.h"
+
+
+
+Ciudad::Ciudad(int identificador)
+{
+	id = identificador;
+	nombre = "";
+	for each (int distancia_a in distancia)
+	{
+		distancia_a = -1;
+	}
+}
+
+Ciudad::Ciudad(char *nombre_ciudad,int identificador)
+{
+	id = identificador;
+	nombre = nombre_ciudad;
+	for (int i = 0; i < N; i++)
+	{
+		distancia[i] = -1;
+	}
+}
+
+void Ciudad::asignardistancia(int dist, int ciudad)
+{
+	if (ciudad >= N)
+		return;
+	distancia[ciudad] = dist;
+}
+
+int Ciudad::obtenerdistancia(int ciudad)
+{
+	return distancia[ciudad];
+}
+
+
+
+Ciudad::~Ciudad()
+{
+}
---
+++ Tree search/Tree search/Ciudad.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdafx.h"
+class Ciudad
+{
+private:
+	
+	int distancia[N];
+	char* nombre;
+public:
+	int id;
+	Ciudad(int);
+	Ciudad(char*, int);
+	void asignardistancia(int dist, int ciudad);
+	int obtenerdistancia(int ciudad);
+	~Ciudad();
+};
+
